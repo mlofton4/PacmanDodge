1a8d6b4210c7f8febe36f1d807a8279714e5d317
leoneljerez <leoneljerez1022@gmail.com>
2019-10-08 09:39:53 -0400

    Pac-Dots will spawn programmatically instead.

---
+++ Assets/Scripts/GeneratePacdots.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class GeneratePacdots : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 offset = new Vector3(0f, 0f, 0f);
+        cube.GetComponent<Renderer>().enabled = false;
+        Instantiate(prefab, cube.transform.position + offset, Quaternion.identity);
+    }
+}
